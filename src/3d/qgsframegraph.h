45bf156b1819d4356dd83e84ffbb0e91df92ed49
Jean Felder <jean.felder@oslandia.com>
2024-06-21 21:45:00 +0200

    qgsframegraph: Add logic to dump framegraph as string
    Co-authored-by: Mike Krus <mike.krus@kdab.com>

--- src/3d/qgsframegraph.h
+++ src/3d/qgsframegraph.h
@@ -216,6 +216,12 @@ class QgsFrameGraph : public Qt3DCore::QEntity
      */
     void setDebugOverlayEnabled( bool enabled );
 
+    //! Dump frame graph as string
+    QString dumpFrameGraph() const;
+
+    //! Dump scene graph as string
+    QString dumpSceneGraph() const;
+
   private:
     Qt3DRender::QRenderSurfaceSelector *mRenderSurfaceSelector = nullptr;
     Qt3DRender::QViewport *mMainViewPort = nullptr;
