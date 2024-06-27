Oslandia/QGIS_CI

feat(3d/renderView): add target render output
* remove useless setRootEntity
* add renderview names

src/3d/qgs3daxisrenderview.cpp
@@ -44,14 +44,15 @@ Qgs3DAxisRenderView::Qgs3DAxisRenderView( QObject *parent, Qt3DExtras::Qt3DWindo
   , mAxisCamera( axisCamera )
   , mMapSettings( settings )
 {
-  mShadowRendererEnabler = new Qt3DRender::QSubtreeEnabler;
-  mShadowRendererEnabler->setEnablement( Qt3DRender::QSubtreeEnabler::Persistent );
+  mRendererEnabler = new Qt3DRender::QSubtreeEnabler;
+  mRendererEnabler->setEnablement( Qt3DRender::QSubtreeEnabler::Persistent );
 
-  mAxisViewport = new Qt3DRender::QViewport( mShadowRendererEnabler );
+  mAxisViewport = new Qt3DRender::QViewport( mRendererEnabler );
 
   mAxisSceneLayer = new Qt3DRender::QLayer;
   mAxisSceneLayer->setRecursive( true );
 
+  // render pass
   auto axisLayerFilter = new Qt3DRender::QLayerFilter( mAxisViewport );
   axisLayerFilter->addLayer( mAxisSceneLayer );
 
@@ -59,37 +60,49 @@ Qgs3DAxisRenderView::Qgs3DAxisRenderView( QObject *parent, Qt3DExtras::Qt3DWindo
   axisCameraSelector->setParent( axisLayerFilter );
   axisCameraSelector->setCamera( mAxisCamera );
 
-  auto clearBuffers = new Qt3DRender::QClearBuffers( axisCameraSelector );
+  mRenderTargetSelector = new Qt3DRender::QRenderTargetSelector( axisCameraSelector );
+  // no target output for now, updateTargetOutput() will be called later
+
+  auto clearBuffers = new Qt3DRender::QClearBuffers( mRenderTargetSelector );
   clearBuffers->setBuffers( Qt3DRender::QClearBuffers::DepthBuffer );
 
+  // update viewport size
   onAxisViewportSizeUpdate();
 
   connect( mParentWindow, &Qt3DExtras::Qt3DWindow::widthChanged, this, &Qgs3DAxisRenderView::onAxisViewportSizeUpdate );
   connect( mParentWindow, &Qt3DExtras::Qt3DWindow::heightChanged, this, &Qgs3DAxisRenderView::onAxisViewportSizeUpdate );
 }
 
-//! Sets root entity of the 3D scene
-void Qgs3DAxisRenderView::setRootEntity( Qt3DCore::QEntity *root )
+void Qgs3DAxisRenderView::onTargetOutputUpdate()
 {
+  if ( ! mTargetOutputs.isEmpty() && mRenderTargetSelector )
+  {
+    Qt3DRender::QRenderTarget *renderTarget = new Qt3DRender::QRenderTarget;
+
+    for ( Qt3DRender::QRenderTargetOutput *targetOutput : qAsConst( mTargetOutputs ) )
+      renderTarget->addOutput( targetOutput );
+
+    mRenderTargetSelector->setTarget( renderTarget );
+  }
 }
 
 void Qgs3DAxisRenderView::enableSubTree( bool enable )
 {
-  if ( mShadowRendererEnabler != nullptr )
+  if ( mRendererEnabler != nullptr )
   {
-    mShadowRendererEnabler->setEnabled( enable );
+    mRendererEnabler->setEnabled( enable );
   }
 }
 
 bool Qgs3DAxisRenderView::isSubTreeEnabled()
 {
-  return mShadowRendererEnabler != nullptr && mShadowRendererEnabler->isEnabled();
+  return mRendererEnabler != nullptr && mRendererEnabler->isEnabled();
 }
 
 
 Qt3DRender::QFrameGraphNode *Qgs3DAxisRenderView::topGraphNode()
 {
-  return mShadowRendererEnabler;
+  return mRendererEnabler;
 }
 
 Qt3DRender::QViewport *Qgs3DAxisRenderView::viewport()

src/3d/qgsshadowrenderview.cpp
@@ -34,25 +34,31 @@
 #include <Qt3DRender/QRenderCapture>
 #include <Qt3DRender/QSubtreeEnabler>
 
-QgsShadowRenderView::QgsShadowRenderView( QObject *parent, Qt3DRender::QRenderTargetOutput *targetOutput )
+QgsShadowRenderView::QgsShadowRenderView( QObject *parent )
   : QgsAbstractRenderView( parent )
-  , mTargetOutput( targetOutput )
 {
-
   mLightCamera = new Qt3DRender::QCamera;
   mCastShadowsLayer = new Qt3DRender::QLayer;
   mCastShadowsLayer->setRecursive( true );
 
-  // shadow rendering pass
   mShadowRendererEnabler = new Qt3DRender::QSubtreeEnabler;
   mShadowRendererEnabler->setEnablement( Qt3DRender::QSubtreeEnabler::Persistent );
 
+  // shadow rendering pass
   constructShadowRenderPass();
 }
 
-void QgsShadowRenderView::setRootEntity( Qt3DCore::QEntity *root )
+void QgsShadowRenderView::onTargetOutputUpdate()
 {
+  if ( ! mTargetOutputs.isEmpty() && mShadowRenderTargetSelector )
+  {
+    Qt3DRender::QRenderTarget *shadowRenderTarget = new Qt3DRender::QRenderTarget;
 
+    for ( Qt3DRender::QRenderTargetOutput *targetOutput : qAsConst( mTargetOutputs ) )
+      shadowRenderTarget->addOutput( targetOutput );
+
+    mShadowRenderTargetSelector->setTarget( shadowRenderTarget );
+  }
 }
 
 Qt3DRender::QLayer *QgsShadowRenderView::layerToFilter()
@@ -122,11 +128,8 @@ Qt3DRender::QFrameGraphNode *QgsShadowRenderView::constructShadowRenderPass()
   mShadowSceneEntitiesFilter = new Qt3DRender::QLayerFilter( mLightCameraSelectorShadowPass );
   mShadowSceneEntitiesFilter->addLayer( mCastShadowsLayer );
 
-  Qt3DRender::QRenderTarget *shadowRenderTarget = new Qt3DRender::QRenderTarget;
-  shadowRenderTarget->addOutput( mTargetOutput );
-
   mShadowRenderTargetSelector = new Qt3DRender::QRenderTargetSelector( mShadowSceneEntitiesFilter );
-  mShadowRenderTargetSelector->setTarget( shadowRenderTarget );
+  // no target output for now, updateTargetOutput() will be called later
 
   mShadowClearBuffers = new Qt3DRender::QClearBuffers( mShadowRenderTargetSelector );
   mShadowClearBuffers->setBuffers( Qt3DRender::QClearBuffers::BufferType::ColorDepthBuffer );

benoitdm-oslandia/QGIS

src/3d/qgs3daxisrenderview.cpp
@@ -68,9 +68,12 @@ Qgs3DAxisRenderView::Qgs3DAxisRenderView( QObject *parent, Qgs3DMapCanvas *canva
   Qt3DRender::QCameraSelector *cameraSelector = new Qt3DRender::QCameraSelector( objectFilter );
   cameraSelector->setCamera( mObjectCamera );
 
+  mRenderTargetSelector = new Qt3DRender::QRenderTargetSelector( cameraSelector );
+  // no target output for now, updateTargetOutput() will be called later
+
   // This ensures to have the labels (Text2DEntity) rendered after the other objects and therefore
   // avoid any transparency issue on the labels.
-  Qt3DRender::QSortPolicy *objectSortPolicy = new Qt3DRender::QSortPolicy( cameraSelector );
+  Qt3DRender::QSortPolicy *objectSortPolicy = new Qt3DRender::QSortPolicy( mRenderTargetSelector );
   QVector<Qt3DRender::QSortPolicy::SortType> objectSortTypes = QVector<Qt3DRender::QSortPolicy::SortType>();
   objectSortTypes << Qt3DRender::QSortPolicy::BackToFront;
   objectSortPolicy->setSortTypes( objectSortTypes );
@@ -102,6 +105,19 @@ Qgs3DAxisRenderView::Qgs3DAxisRenderView( QObject *parent, Qgs3DMapCanvas *canva
 
   connect( mCanvas, &Qgs3DMapCanvas::widthChanged, this, &Qgs3DAxisRenderView::onViewportSizeUpdate );
   connect( mCanvas, &Qgs3DMapCanvas::heightChanged, this, &Qgs3DAxisRenderView::onViewportSizeUpdate );
+}
+
+void Qgs3DAxisRenderView::onTargetOutputUpdate()
+{
+  if ( ! mTargetOutputs.isEmpty() && mRenderTargetSelector )
+  {
+    Qt3DRender::QRenderTarget *renderTarget = new Qt3DRender::QRenderTarget;
+
+    for ( Qt3DRender::QRenderTargetOutput *targetOutput : qAsConst( mTargetOutputs ) )
+      renderTarget->addOutput( targetOutput );
+
+    mRenderTargetSelector->setTarget( renderTarget );
+  }
 }
 
 void Qgs3DAxisRenderView::enableSubTree( bool enable )

src/3d/qgsshadowrenderview.cpp
@@ -33,11 +33,9 @@
 #include <Qt3DRender/QPolygonOffset>
 #include <Qt3DRender/QSubtreeEnabler>
 
-QgsShadowRenderView::QgsShadowRenderView( QObject *parent, Qt3DRender::QRenderTargetOutput *targetOutput )
+QgsShadowRenderView::QgsShadowRenderView( QObject *parent )
   : QgsAbstractRenderView( parent )
-  , mTargetOutput( targetOutput )
 {
-
   mLightCamera = new Qt3DRender::QCamera;
   mLightCamera->setObjectName( "QgsShadowRenderView::LightCamera" );
   mLayer = new Qt3DRender::QLayer;
@@ -50,6 +48,20 @@ QgsShadowRenderView::QgsShadowRenderView( QObject *parent, Qt3DRender::QRenderTa
 
   // shadow rendering pass
   buildRenderPass();
+}
+
+void QgsShadowRenderView::onTargetOutputUpdate()
+{
+  if ( ! mTargetOutputs.isEmpty() && mRenderTargetSelector )
+  {
+    Qt3DRender::QRenderTarget *renderTarget = new Qt3DRender::QRenderTarget;
+    renderTarget->setObjectName( "QgsShadowRenderView::Target" );
+
+    for ( Qt3DRender::QRenderTargetOutput *targetOutput : qAsConst( mTargetOutputs ) )
+      renderTarget->addOutput( targetOutput );
+
+    mRenderTargetSelector->setTarget( renderTarget );
+  }
 }
 
 Qt3DRender::QLayer *QgsShadowRenderView::layerToFilter()
@@ -120,11 +132,8 @@ Qt3DRender::QFrameGraphNode *QgsShadowRenderView::buildRenderPass()
   mLayerFilter = new Qt3DRender::QLayerFilter( mLightCameraSelector );
   mLayerFilter->addLayer( mLayer );
 
-  Qt3DRender::QRenderTarget *renderTarget = new Qt3DRender::QRenderTarget;
-  renderTarget->addOutput( mTargetOutput );
-
   mRenderTargetSelector = new Qt3DRender::QRenderTargetSelector( mLayerFilter );
-  mRenderTargetSelector->setTarget( renderTarget );
+  // no target output for now, updateTargetOutput() will be called later
 
   mClearBuffers = new Qt3DRender::QClearBuffers( mRenderTargetSelector );
   mClearBuffers->setBuffers( Qt3DRender::QClearBuffers::BufferType::ColorDepthBuffer );