Oslandia/QGIS_CI

feat(3d/renderview): extract forward render view

src/3d/qgsoffscreen3dengine.cpp
@@ -144,7 +144,8 @@ void QgsOffscreen3DEngine::setRootEntity( Qt3DCore::QEntity *root )
   // Parent the incoming scene root to our current root entity.
   mSceneRoot = root;
   mSceneRoot->setParent( mRoot );
-  root->addComponent( mFrameGraph->forwardRenderLayer() );
+  if ( mFrameGraph->renderView( QgsShadowRenderingFrameGraph::FORWARD_RENDERVIEW ) )
+    root->addComponent( mFrameGraph->renderView( QgsShadowRenderingFrameGraph::FORWARD_RENDERVIEW )->layerToFilter() );
   if ( mFrameGraph->renderView( QgsShadowRenderingFrameGraph::SHADOW_RENDERVIEW ) )
     root->addComponent( mFrameGraph->renderView( QgsShadowRenderingFrameGraph::SHADOW_RENDERVIEW )->layerToFilter() );
 }

src/3d/qgswindow3dengine.cpp
@@ -64,7 +64,8 @@ void QgsWindow3DEngine::setRootEntity( Qt3DCore::QEntity *root )
 {
   mSceneRoot = root;
   mSceneRoot->setParent( mRoot );
-  mSceneRoot->addComponent( mFrameGraph->forwardRenderLayer() );
+  if ( mFrameGraph->renderView( QgsShadowRenderingFrameGraph::FORWARD_RENDERVIEW ) )
+    mSceneRoot->addComponent( mFrameGraph->renderView( QgsShadowRenderingFrameGraph::FORWARD_RENDERVIEW )->layerToFilter() );
   if ( mFrameGraph->renderView( QgsShadowRenderingFrameGraph::SHADOW_RENDERVIEW ) )
     mSceneRoot->addComponent( mFrameGraph->renderView( QgsShadowRenderingFrameGraph::SHADOW_RENDERVIEW )->layerToFilter() );
 }

benoitdm-oslandia/QGIS

src/3d/qgsoffscreen3dengine.cpp
@@ -149,7 +149,8 @@ void QgsOffscreen3DEngine::setRootEntity( Qt3DCore::QEntity *root )
   // Parent the incoming scene root to our current root entity.
   mSceneRoot = root;
   mSceneRoot->setParent( mRoot );
-  root->addComponent( mFrameGraph->forwardRenderLayer() );
+  if ( mFrameGraph->renderView( QgsFrameGraph::FORWARD_RENDERVIEW ) )
+    root->addComponent( mFrameGraph->renderView( QgsFrameGraph::FORWARD_RENDERVIEW )->layerToFilter() );
   if ( mFrameGraph->renderView( QgsFrameGraph::SHADOW_RENDERVIEW ) )
     root->addComponent( mFrameGraph->renderView( QgsFrameGraph::SHADOW_RENDERVIEW )->layerToFilter() );
 }

src/3d/qgswindow3dengine.cpp
@@ -65,7 +65,8 @@ void QgsWindow3DEngine::setRootEntity( Qt3DCore::QEntity *root )
 {
   mSceneRoot = root;
   mSceneRoot->setParent( mRoot );
-  mSceneRoot->addComponent( mFrameGraph->forwardRenderLayer() );
+  if ( mFrameGraph->renderView( QgsFrameGraph::FORWARD_RENDERVIEW ) )
+    mSceneRoot->addComponent( mFrameGraph->renderView( QgsFrameGraph::FORWARD_RENDERVIEW )->layerToFilter() );
   if ( mFrameGraph->renderView( QgsFrameGraph::SHADOW_RENDERVIEW ) )
     mSceneRoot->addComponent( mFrameGraph->renderView( QgsFrameGraph::SHADOW_RENDERVIEW )->layerToFilter() );
 }