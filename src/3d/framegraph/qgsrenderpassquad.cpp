9e1ccca47da5396613c00bd17f42e071f8c99825
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2024-08-30 15:20:23 +0200

    fix(qgsrenderpassquad): parent need to be set at the constructor end.

--- src/3d/framegraph/qgsrenderpassquad.cpp
+++ src/3d/framegraph/qgsrenderpassquad.cpp
@@ -44,7 +44,8 @@ typedef Qt3DCore::QGeometry Qt3DQGeometry;
 #include <QVector3D>
 
 QgsRenderPassQuad::QgsRenderPassQuad( Qt3DRender::QLayer *layer, QNode *parent )
-  : Qt3DCore::QEntity( parent )
+  : Qt3DCore::QEntity() // Qt3D bug: if we set the parent in the super constructor
+    // the entity is not attached properly. We attach it at the constructor end.
 {
   Qt3DQGeometry *geom = new Qt3DQGeometry( this );
   Qt3DQAttribute *positionAttribute = new Qt3DQAttribute( this );
@@ -99,4 +100,6 @@ QgsRenderPassQuad::QgsRenderPassQuad( Qt3DRender::QLayer *layer, QNode *parent )
   addComponent( mMaterial );
 
   addComponent( layer );
+
+  setParent( parent ); // Needed!
 }
