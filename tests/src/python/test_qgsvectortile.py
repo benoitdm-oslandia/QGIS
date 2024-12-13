2b366b066e09285809c48041953324ac74f149a5
Max Tobias Weber <tobias.weber@wheregroup.com>
2024-12-13 15:36:03 +0100

    fix test tests/src/python/test_qgsvectortile.py

--- tests/src/python/test_qgsvectortile.py
+++ tests/src/python/test_qgsvectortile.py
@@ -125,7 +125,7 @@ class TestVectorTile(QgisTestCase):
         uri = md.encodeUri(parts)
         self.assertEqual(
             uri,
-            "type=xyz&url=https://fake.new.server/%7Bx%7D/%7By%7D/%7Bz%7D.png&zmax=2&zmin=0",
+            "type=xyz&url=https%3A%2F%2Ffake.new.server%2F%7Bx%7D%2F%7By%7D%2F%7Bz%7D.png&zmax=2&zmin=0",
         )
 
         uri = "type=xyz&serviceType=arcgis&url=https://fake.server/%7Bx%7D/%7By%7D/%7Bz%7D.png&zmax=2&http-header:referer=https://qgis.org/&styleUrl=https://qgis.org/"
